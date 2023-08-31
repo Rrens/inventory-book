61089d263192707a2ac1c7efb846651df85c37f9
Rrens <yusufrendi313@gmail.com>
2023-09-01 01:47:10 +0700

    Fix Print

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\DetailPeminjaman;
 use App\Models\Peminjaman;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -13,14 +14,25 @@ class LaporanController extends Controller
     public function index()
     {
         $active = 'laporan';
-        $data = DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
-            ->groupBy('id_user')
+        $data = DB::table('detail_peminjaman as dp')
+            ->join('peminjaman as p', 'p.id', '=', 'dp.id_peminjaman')
+            ->groupBy('p.tgl_pinjam')
             ->get();
+
+        $for_count =
+            DB::table('detail_peminjaman as dp')
+            ->select('p.tgl_pinjam', 'dp.keterangan')
+            ->join('peminjaman as p', 'p.id', '=', 'dp.id_peminjaman')
+            ->get();
+
         $data_detail =
             DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
             ->get();
-        // dd($data);
-        return view('admin.page.laporan', compact('active', 'data', 'data_detail'));
+
+        foreach ($data_detail as $item) {
+            $item->tanggal = $item->Peminjaman[0]->tgl_pinjam;
+        }
+        return view('admin.page.laporan', compact('active', 'data', 'data_detail', 'for_count'));
     }
 
     public function filter(Request $request)
@@ -35,60 +47,52 @@ class LaporanController extends Controller
             return back();
         }
 
-        $check_date = Peminjaman::where('tgl_pinjam', $request->date)
-            ->select('id')
+        $for_count =
+            DB::table('detail_peminjaman as dp')
+            ->select('p.tgl_pinjam', 'dp.keterangan')
+            ->join('peminjaman as p', 'p.id', '=', 'dp.id_peminjaman')
             ->get();
-        // $date = Peminjaman::all();
-
-        $for_id = array();
-        foreach ($check_date as $key) {
-            array_push($for_id, $key->id);
-        }
-        // dd($for_id);
-        $data = DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
-            ->whereIn('id_peminjaman', $for_id)
-            ->groupBy('id_user')
+        $data = DB::table('detail_peminjaman as dp')
+            ->join('peminjaman as p', 'p.id', '=', 'dp.id_peminjaman')
+            ->where('p.tgl_pinjam', $request->date)
+            ->groupBy('p.tgl_pinjam')
             ->get();
-
         $data_detail =
             DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
-            ->whereIn('id_peminjaman', $for_id)
             ->get();
+
+        foreach ($data_detail as $item) {
+            $item->tanggal = $item->Peminjaman[0]->tgl_pinjam;
+        }
+
         $date = $request->date;
 
-        return view('admin.page.filter-laporan', compact('active', 'data', 'date', 'data_detail'));
+        return view('admin.page.filter-laporan', compact('active', 'data', 'date', 'data_detail', 'for_count'));
     }
 
     public function print_laporan(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'date' => 'date',
-            'id_user' => 'required'
         ]);
 
         if ($validator->fails()) {
             Alert::error($validator->messages()->all());
             return back();
         }
-        if ($request->has('date')) {
-            $check_date = Peminjaman::where('tgl_pinjam', $request->date)
-                ->select('id')
-                ->get();
+        $check_date = Peminjaman::where('tgl_pinjam', $request->date)
+            ->select('id')
+            ->get();
 
-            $for_id = array();
-            foreach ($check_date as $key) {
-                array_push($for_id, $key->id);
-            }
-
-            $data = DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
-                ->where('id_user', $request->id_user)
-                ->whereIn('id_peminjaman', $for_id)
-                ->get();
-        } else {
-            $data = DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
-                ->where('id_user', $request->id_user)
-                ->get();
+        $for_id = array();
+        foreach ($check_date as $key) {
+            array_push($for_id, $key->id);
         }
+
+        $data = DetailPeminjaman::with('User', 'Peminjaman', 'Buku')
+            // ->where('id_user', $request->id_user)
+            ->whereIn('id_peminjaman', $for_id)
+            ->get();
 
         return view('admin.print.print_laporan', compact('data'));
     }
